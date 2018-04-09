04f564110f4ac5d725a21fdbd3802af8703c8088
Luis Landeiro Ribeiro <ribeiro.luis@gmail.com>
2018-04-09 15:35:57 +0100

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,13 @@ import (
 	"strconv"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func handler(w http.ResponseWriter, r *http.Request) {	
+	header := w.Header()
+	csp := []string{"default-src: 'self'", "font-src: 'fonts.googleapis.com'", "frame-src: 'none'"}
+	header := http.Header{
+    		"Content-Type": {"text/html; charset=UTF-8"},
+	}
+	header.Set("Content-Security-Policy", strings.Join(csp, "; "))
 	r.Write(w)
 	fmt.Fprintf(w, "RemoteAddr: %s", r.RemoteAddr)
 }
@@ -21,3 +27,4 @@ func main() {
 	log.Println("Starting http server on port:", *port)
 	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*port), nil))
 }
+
